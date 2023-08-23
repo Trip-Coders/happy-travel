832a4968803d7cdaa547817855083451ee4e7499
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-23 16:09:02 +0200

    Style register/login form

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.nav')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/nav.css') }}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
